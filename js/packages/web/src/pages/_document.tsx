8d9cad092527656f064eddf820cc3c96111e857a
Loic-HAKISA <loic.ehrhart@hakisa.com>
2021-08-25 12:21:50 +0200

    Glubbers icons

--- js/packages/web/src/pages/_document.tsx
+++ js/packages/web/src/pages/_document.tsx
@@ -40,7 +40,7 @@ export default class MetaplexDocument extends Document {
             href="/favicon-16x16.png"
           />
           <meta name="theme-color" content="#000000" />
-          <meta name="description" content="GLUING NFT Marketplace" />
+          <meta name="description" content="Glubbers NFT Marketplace" />
           <link rel="manifest" href="/manifest.json" />
           <link
             rel="stylesheet"
