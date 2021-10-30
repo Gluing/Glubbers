fd26589d919fa61c12af862ef1db8953c8c9cc4c
Loic-HAKISA <loic.ehrhart@hakisa.com>
2021-10-30 13:57:49 +0200

    New glubbers colors

--- js/packages/web/src/views/home/auctionList.tsx
+++ js/packages/web/src/views/home/auctionList.tsx
@@ -141,9 +141,9 @@ export const AuctionListView = () => {
   return (
     <>
       <Banner
-        src={'/main-banner.svg'}
-        headingText={'The amazing world of Metaplex.'}
-        subHeadingText={'Buy exclusive Metaplex NFTs.'}
+        src={'/glubbers-banner.svg'}
+        headingText={'The amazing world of GLUING metaverse.'}
+        subHeadingText={'Buy exclusive Glubbers NFTs.'}
         actionComponent={<HowToBuyModal buttonClassName="secondary-btn" />}
         useBannerBg={true}
       />
--- js/packages/web/src/views/staticPage/staticData.js
+++ js/packages/web/src/views/staticPage/staticData.js
@@ -2,7 +2,7 @@ export const data = {
   headContent: {
     title: 'We are Metaplex',
     subtitle: 'About Us',
-    bannerImage: '/main-banner.png',
+    bannerImage: '/glubbers-banner.png',
   },
   leftContent: undefined,
   midContent: {
@@ -14,7 +14,7 @@ export const data = {
           '• Starting a variety of auctions for primary/secondary sales;',
           '• and Visualizing NFTs in a standard way across wallets and applications.',
         ],
-        image: '/main-banner.png',
+        image: '/glubbers-banner.png',
       },
     ],
   },
